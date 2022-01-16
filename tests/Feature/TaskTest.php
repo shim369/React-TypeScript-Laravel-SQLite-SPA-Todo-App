abaadd339a693fc01dda911260e781e70d9b4582
shimpei ohtani <ohtani.shimpei@gmail.com>
2022-01-16 17:43:18 +0900

    UnitTestの設定

---
+++ tests/Feature/TaskTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\Task;
+
+class TaskTest extends TestCase
+{
+    use RefreshDatabase;//テスト実行時にデータベースをリセットする
+    /**
+     * @test
+     */
+    public function 一覧を取得()
+    {
+        $tasks = Task::factory()->count(10)->create();
+
+        $response = $this->getJson('api/tasks');
+
+        $response
+            ->assertOk()
+            ->assertJsonCount($tasks->count());
+    }
+}
